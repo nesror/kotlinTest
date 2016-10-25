6d01e04641fb38a939c3604b3d62aed50f383af6
nestor <nestor@yzapp.cn>
2016-10-25 19:25:30 +0800

    no message

--- app/src/main/java/cn/yzapp/kotlintest/data/Data.kt
+++ app/src/main/java/cn/yzapp/kotlintest/data/Data.kt
@@ -1,27 +1,16 @@
 package cn.yzapp.kotlintest.data
 
-import android.util.Log
 import java.util.*
 import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
 
 /**
  * @author: GuSiheng
  * 委托不能放在bean里不然Gson转换无效
  */
-class User(var id: Long) {
+class User(var id: Long){
     // 使用委托后Gson转换无效！！
-    var name: String by Delegates.vetoable("Incubator") {
-        d, old, new ->
-        Log.d(javaClass.simpleName, new)
-        new.length > 1
-    }
-
-    /*var nameJson: String = ""
-        get() = name
-        set(value) {
-            field = value
-            name = value
-        }*/
+    var name: String by MyDelegate()
 
     var books = ArrayList<Book>()
 
@@ -32,6 +21,17 @@ class User(var id: Long) {
     var showChineseName by Delegates.observable(false) {
         d, old, new ->
         name = if (new) "丘比" else "Incubator"
+    }
+}
+
+class MyDelegate() {
+    operator fun getValue(thisRef: Any?, prop: KProperty<*>): String {
+        if(prop.)
+        return "$thisRef, thank you for delegating '${prop}' to me!"
+    }
+
+    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: String) {
+        println("$value has been assigned to ${prop.name} in $thisRef")
     }
 }
 
